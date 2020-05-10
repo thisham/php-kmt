10c426a9765842b43e6b7b6be3f9cfb0192a5788
lukistraadr <59078748+lukistraadr@users.noreply.github.com>
2020-05-10 21:13:14 +0700

    Tambahan helper + pustaka

--- app/inti/Kontroler.php
+++ app/inti/Kontroler.php
@@ -16,6 +16,17 @@ class Kontroler
 		require_once '../app/model/' . $model . '.php';
 		return new $model;
 	}
+	
+	public function pustaka($pustaka)
+	{
+		require_once '../app/pustaka/' . $pustaka . '.php';
+		return new $pustaka;
+	}
+	
+	public function helper($helper)
+	{
+		require_once '../app/helper/' . $helper . '.php';
+	}
 }
 
 ?>
