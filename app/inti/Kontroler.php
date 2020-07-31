a4f0b98631946b46a67ccf9ade7848689b9b7cf1
Hamdan Yuwafi <yuwafi.hamdan365@gmail.com>
2020-07-31 17:38:13 +0700

    Update Kontroler.php

--- app/inti/Kontroler.php
+++ app/inti/Kontroler.php
@@ -8,24 +8,24 @@ class Kontroler
 	
 	public function tampilkan($view, $data = [])
 	{
-		require_once '../app/tampilan/' . $view . '.php';
+		require_once DIREKTORI . '/app/tampilan/' . $view . '.php';
 	}
 
 	public function model($model)
 	{
-		require_once '../app/model/' . $model . '.php';
+		require_once DIREKTORI . '/app/model/' . $model . '.php';
 		return new $model;
 	}
 	
 	public function pustaka($pustaka)
 	{
-		require_once '../app/pustaka/' . $pustaka . '.php';
+		require_once DIREKTORI . '/app/pustaka/' . $pustaka . '.php';
 		return new $pustaka;
 	}
 	
 	public function helper($helper)
 	{
-		require_once '../app/helper/' . $helper . '.php';
+		require_once DIREKTORI . '/app/helper/' . $helper . '.php';
 	}
 }
 
